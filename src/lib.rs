48e25ffdb2423734059a62f303979ed4e8fc6cc6
FloRide1 <florian.reimat@orange.fr>
2024-09-10 11:38:11 +0200

    feat(lib): Create basic app router

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![warn(missing_docs)]
+//! scrounch_backend - Backend for a sales application
+//!
+//! This library serves as the core of the `scrounch_backend` crate, providing
+//! all backend functionality for managing beverage sales. It integrates the Axum
+//! framework for handling HTTP requests and response routing.
+
+mod routes;
+
+/// Creates and configures the Axum application.
+///
+/// This function sets up the Axum app, defines the routes, middleware,
+/// and any other necessary configuration for handling HTTP requests.
+/// It wires up the backend services such as authentication, database connections,
+/// and any other business logic needed to manage the beverage sales system.
+pub async fn app() -> axum::Router {
+    axum::Router::new().route("/status", axum::routing::get(routes::status::get_status))
+}
