d296be1acc1bee92a5b9b7771279556218bbd7a1
FloRide1 <florian.reimat@orange.fr>
2024-09-11 11:18:09 +0200

    feat(lib): Add OIDC login + auth layer

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,14 @@
 //! framework for handling HTTP requests and response routing.
 
 mod cli;
+mod oidc;
 mod routes;
 mod state;
 
+use axum::{error_handling::HandleErrorLayer, routing::get};
+use axum_oidc::EmptyAdditionalClaims;
 pub use cli::Arguments;
+use oidc::handle_axum_oidc_middleware_error;
 
 /// Creates and configures the Axum application.
 ///
@@ -18,10 +22,48 @@ pub use cli::Arguments;
 /// It wires up the backend services such as authentication, database connections,
 /// and any other business logic needed to manage the beverage sales system.
 pub async fn app(arguments: Arguments) -> axum::Router {
-    let state = state::AppState { arguments };
+    let state = state::AppState {
+        arguments: arguments.clone(),
+    };
+
+    let login_service = tower::ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(handle_axum_oidc_middleware_error))
+        .layer(axum_oidc::OidcLoginLayer::<EmptyAdditionalClaims>::new());
+
+    let oidc_client = oidc::get_oidc_client(&arguments)
+        .await
+        .expect("Can't create OIDC client");
+
+    let auth_service = tower::ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(handle_axum_oidc_middleware_error))
+        .layer(oidc_client);
 
     axum::Router::new()
+        .merge(auth_required_routes())
+        .layer(login_service)
+        .merge(auth_optional_routes())
+        .layer(auth_service)
+        .layer(oidc::memory_session_layer())
         .merge(routes::openapi::openapi())
-        .route("/status", axum::routing::get(routes::status::get_status))
+        .route("/status", get(routes::status::get_status))
         .with_state(state)
+}
+
+/// Defines routes that require user authentication.
+///
+/// This function creates an `axum::Router` specifically for routes that are
+/// protected by authentication. These routes require the user to be logged in
+/// and authenticated via OpenID Connect (OIDC) to access, otherwise it redirect them to the OIDC
+/// login page.
+fn auth_required_routes() -> axum::Router<state::AppState> {
+    axum::Router::new()
+}
+
+/// Defines routes that do not require user authentication.
+///
+/// This function creates an `axum::Router` for routes that can be accessed
+/// without user authentication. These routes are publicly accessible and do not
+/// require OpenID Connect (OIDC) login.
+fn auth_optional_routes() -> axum::Router<state::AppState> {
+    axum::Router::new()
 }
---
+++ src/oidc.rs
@@ -0,0 +1,128 @@
+//! OpenID Connect (OIDC) utilities and configuration.
+//!
+//! This module provides the necessary utilities for setting up and managing OpenID Connect (OIDC)
+//! authentication in the `scrounch_backend` application. It includes functions for creating an OIDC
+//! client, managing sessions, and handling authentication layers in the Axum framework.
+
+use std::str::FromStr;
+
+use axum::{extract::FromRequestParts, response::IntoResponse};
+use axum_oidc::{
+    error::{ExtractorError, MiddlewareError},
+    EmptyAdditionalClaims, OidcClaims,
+};
+
+/// Provides a session layer for managing user sessions.
+///
+/// This function returns a session layer based on an in-memory store. The session layer
+/// is used to manage user authentication sessions in the application, allowing the
+/// server to store session data such as authentication tokens.
+pub fn memory_session_layer() -> tower_sessions::SessionManagerLayer<tower_sessions::MemoryStore> {
+    let session_store = tower_sessions::MemoryStore::default();
+    tower_sessions::SessionManagerLayer::new(session_store)
+        .with_secure(false)
+        .with_same_site(tower_sessions::cookie::SameSite::Lax)
+        .with_expiry(tower_sessions::Expiry::OnInactivity(
+            tower_sessions::cookie::time::Duration::seconds(120),
+        ))
+}
+
+/// Creates an OIDC client for handling authentication.
+///
+/// This function initializes an OpenID Connect client
+pub async fn get_oidc_client(
+    arguments: &crate::Arguments,
+) -> Option<axum_oidc::OidcAuthLayer<EmptyAdditionalClaims>> {
+    let backend_base_url = axum::http::Uri::from_str(&arguments.backend_base_url)
+        .expect("Backend base url is not valid");
+    let issuer = arguments.openid_issuer.to_owned();
+    let client_id = arguments.openid_client_id.to_owned();
+    let client_secret = arguments.openid_client_secret.to_owned();
+
+    axum_oidc::OidcAuthLayer::<EmptyAdditionalClaims>::discover_client(
+        backend_base_url,
+        issuer,
+        client_id,
+        client_secret,
+        vec![
+            "openid".to_string(),
+            "email".to_string(),
+            "profile".to_string(),
+        ],
+    )
+    .await
+    .ok()
+}
+
+/// Handles errors encountered in the OpenID Connect (OIDC) middleware.
+///
+/// This function provides a centralized way to handle errors that occur in the
+/// OIDC authentication middleware. It takes a `MiddlewareError`, which may occur
+/// during login, token validation, or other OIDC-related operations, and converts
+/// it into an appropriate HTTP response.
+pub async fn handle_axum_oidc_middleware_error(
+    e: MiddlewareError,
+) -> axum::http::Response<axum::body::Body> {
+    e.into_response()
+}
+
+/// Represents an authenticated OpenID Connect (OIDC) user.
+///
+/// This struct holds the basic user information retrieved from an OIDC provider
+/// after a successful login. It contains identifying details such as the user's
+/// ID, username, name, and email address.
+#[derive(Debug, Default, Clone)]
+pub struct OidcUser {
+    pub id: String,
+    pub username: String,
+    pub name: String,
+    pub email: String,
+}
+
+/// Extracts an `OidcUser` from the request parts.
+///
+/// This implementation enables the extraction of the `OidcUser` struct from incoming
+/// HTTP request parts using Axum's `FromRequestParts` trait. The user information is
+/// retrieved from the OpenID Connect (OIDC) claims, and the required fields (ID, username,
+/// name, and email) are extracted from the OIDC token claims.
+#[axum::async_trait]
+impl<S> FromRequestParts<S> for OidcUser
+where
+    S: Send + Sync,
+{
+    type Rejection = <OidcClaims<EmptyAdditionalClaims> as FromRequestParts<
+        OidcClaims<EmptyAdditionalClaims>,
+    >>::Rejection;
+
+    async fn from_request_parts(
+        parts: &mut axum::http::request::Parts,
+        state: &S,
+    ) -> Result<Self, Self::Rejection> {
+        let extractor =
+            OidcClaims::<EmptyAdditionalClaims>::from_request_parts(parts, state).await?;
+        let id = extractor.subject().to_string();
+
+        let username = extractor
+            .preferred_username()
+            .ok_or(ExtractorError::Unauthorized)?
+            .to_string();
+        let name = extractor
+            .name()
+            .ok_or(ExtractorError::Unauthorized)?
+            .get(None)
+            .expect("Name is not in correct Langage")
+            .to_string();
+        let email = extractor
+            .email()
+            .ok_or(ExtractorError::Unauthorized)?
+            .to_string();
+
+        let user = OidcUser {
+            id,
+            username,
+            name,
+            email,
+        };
+        Ok(user)
+    }
+}
