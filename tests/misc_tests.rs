5afc0a5d9d256bf0fd992e283343345d7aa4a834
FloRide1 <florian.reimat@orange.fr>
2024-09-15 17:31:10 +0200

    fix(tests): Merge openapi_tests and status_tests to misc_tests

--- tests/misc_tests.rs
+++ tests/misc_tests.rs
@@ -77,3 +77,50 @@ async fn basic_swagger_test() {
         assert_eq!(response.status(), StatusCode::NOT_FOUND);
     }
 }
+
+#[tokio::test]
+async fn basic_status_test() {
+    let realm_name = "master";
+    let basic_client = Client {
+        client_id: "scrouch-backend-example-basic".to_string(),
+        client_secret: Some("123456".to_string()),
+        ..Default::default()
+    };
+
+    let keycloak = Keycloak::start(vec![Realm {
+        name: realm_name.to_string(),
+        users: vec![],
+        clients: vec![basic_client.clone()],
+    }])
+    .await
+    .unwrap();
+
+    let url = keycloak.url();
+
+    let mut arguments = scrounch_backend::Arguments::default();
+    arguments.openid_issuer = format!("{url}/realms/{realm_name}");
+    arguments.openid_client_id = basic_client.client_id;
+    arguments.openid_client_secret = basic_client.client_secret;
+    arguments.backend_base_url = "http://localhost:3000".to_string();
+    arguments.frontend_base_url = "http://localhost:5173".to_string();
+
+    let app = app(arguments).await;
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .uri("/status")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body = std::str::from_utf8(&body).unwrap();
+
+    assert_eq!(body, "UP");
+}
