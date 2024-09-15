9ce1771b094a4374adf1d0e6c94123a4bb292d73
FloRide1 <florian.reimat@orange.fr>
2024-09-15 17:22:34 +0200

    fix(tests): Add OIDC to openapi_tests and status_tests

--- tests/openapi_tests.rs
+++ tests/openapi_tests.rs
@@ -1,12 +1,38 @@
+mod utils;
+
 use axum::{body::Body, extract::Request, http::StatusCode};
 use http_body_util::BodyExt;
 use scrounch_backend::app;
 use serde_json::{json, Value};
 use tower::util::ServiceExt;
 
-#[tokio::test]
+use crate::utils::containers::keycloak::{Client, Keycloak, Realm};
+
+#[tokio::test(flavor = "multi_thread")]
 async fn basic_swagger_test() {
-    let arguments = scrounch_backend::Arguments::default();
+    let realm_name = "master";
+    let basic_client = Client {
+        client_id: "scrouch-backend-example-basic".to_string(),
+        client_secret: Some("123456".to_string()),
+        ..Default::default()
+    };
+
+    let keycloak = Keycloak::start(vec![Realm {
+        name: realm_name.to_string(),
+        users: vec![],
+        clients: vec![basic_client.clone()],
+    }])
+    .await
+    .unwrap();
+
+    let url = keycloak.url();
+
+    let mut arguments = scrounch_backend::Arguments::default();
+    arguments.openid_issuer = format!("{url}/realms/{realm_name}");
+    arguments.openid_client_id = basic_client.client_id;
+    arguments.openid_client_secret = basic_client.client_secret;
+    arguments.backend_base_url = "http://localhost:3000".to_string();
+    arguments.frontend_base_url = "http://localhost:5173".to_string();
 
     let app = app(arguments).await;
 
