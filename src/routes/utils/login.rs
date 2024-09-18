362cfe310ac50e92b076444997bc981b02321f71
FloRide1 <florian.reimat@orange.fr>
2024-09-18 11:59:25 +0200

    feat(routes/login): Add Db User creation in /login endpoint

--- src/routes/utils/login.rs
+++ src/routes/utils/login.rs
@@ -6,7 +6,10 @@
 //! redirections after successful authentication.
 //!
 
+use crate::error::AppError;
+use crate::{oidc::OidcUser, state::AppState};
 use axum::{extract::State, response::IntoResponse};
+use entity::user::Model as User;
 
 /// Handles the login route by redirecting the user to the frontend.
 ///
@@ -22,6 +25,29 @@ use axum::{extract::State, response::IntoResponse};
             (status = 303, description = "You're logged in, now go back to frontend_base_url")
         )
     )]
-pub async fn get_login(State(arguments): State<crate::cli::Arguments>) -> impl IntoResponse {
-    axum::response::Redirect::to(&arguments.frontend_url)
+pub async fn get_login(
+    user: OidcUser,
+    State(state): State<AppState>,
+) -> Result<impl IntoResponse, AppError> {
+    if service::Query::find_user_by_id(&state.db_pool, user.id.clone())
+        .await?
+        .is_none()
+    {
+        let id = user.id;
+        let uuid =
+            sea_orm::sqlx::types::Uuid::try_parse(&id).map_err(|_| AppError::DatabaseError)?;
+
+        service::Mutation::create_user(
+            &state.db_pool,
+            User {
+                id: uuid,
+                username: user.username,
+                name: user.name,
+                email: user.email,
+            },
+        )
+        .await?;
+    }
+
+    Ok(axum::response::Redirect::to(&state.arguments.frontend_url))
 }
