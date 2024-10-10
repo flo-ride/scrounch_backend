3ab2a0a85cc5dcd8bf2986a17f2b4a20aaecbc1f
FloRide1 <florian.reimat@orange.fr>
2024-10-10 15:43:45 +0200

    feat(migration): Add Product Table (m20220101_000002_create_product_table)

---
+++ migration/src/m20220101_000002_create_product_table.rs
@@ -0,0 +1,49 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .create_table(
+                Table::create()
+                    .table(Product::Table)
+                    .if_not_exists()
+                    .col(uuid(Product::Id).primary_key())
+                    .col(string(Product::Image))
+                    .col(string(Product::Name))
+                    .col(decimal(Product::Price)) // If you ask: why not money ? here is the answer: https://wiki.postgresql.org/wiki/Don't_Do_This#Don.27t_use_money
+                    .col(small_unsigned(Product::Quantity))
+                    .col(small_unsigned_null(Product::MaxQuantityPerCommand))
+                    .col(boolean(Product::Disabled).default(false))
+                    .col(
+                        timestamp_with_time_zone(Product::CreationTime)
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Product::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Product {
+    Table,
+    Id,
+    Image,
+    Name,
+    Price,
+    Quantity,
+    MaxQuantityPerCommand,
+    Disabled,
+    CreationTime,
+}
