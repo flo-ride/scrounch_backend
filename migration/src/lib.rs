de4fda1689bf85d1828f66eafc3d776f24821e53
flo-ride <43076999+flo-ride@users.noreply.github.com>
2024-10-28 22:00:34 +0100

    feat(migration): Add create_location_table migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -4,6 +4,7 @@ mod m20220101_000001_create_user_table;
 mod m20220101_000002_create_product_table;
 mod m20220101_000003_add_is_banned;
 mod m20220101_000004_remove_quantity_from_product;
+mod m20220101_000005_create_location_table;
 
 pub struct Migrator;
 
@@ -15,6 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220101_000002_create_product_table::Migration),
             Box::new(m20220101_000003_add_is_banned::Migration),
             Box::new(m20220101_000004_remove_quantity_from_product::Migration),
+            Box::new(m20220101_000005_create_location_table::Migration),
         ]
     }
 }
---
+++ migration/src/m20220101_000005_create_location_table.rs
@@ -0,0 +1,85 @@
+use sea_orm::{DbBackend, EnumIter, Iterable};
+use sea_orm_migration::{
+    prelude::{extension::postgres::Type, *},
+    schema::*,
+};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        match db.get_database_backend() {
+            DbBackend::MySql | DbBackend::Sqlite => {}
+            DbBackend::Postgres => {
+                manager
+                    .create_type(
+                        Type::create()
+                            .as_enum(LocationCategory)
+                            .values(LocationCategoryVariant::iter())
+                            .to_owned(),
+                    )
+                    .await?;
+            }
+        }
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Location::Table)
+                    .if_not_exists()
+                    .col(uuid(Location::Id).primary_key())
+                    .col(string_uniq(Location::Name))
+                    .col(
+                        timestamp_with_time_zone(Location::CreationTime)
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(enumeration_null(
+                        Location::Category,
+                        Alias::new("location_category"),
+                        LocationCategoryVariant::iter(),
+                    ))
+                    .col(boolean(Location::Disabled).default(false))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Location::Table).to_owned())
+            .await?;
+
+        let db = manager.get_connection();
+        match db.get_database_backend() {
+            DbBackend::MySql | DbBackend::Sqlite => {}
+            DbBackend::Postgres => {
+                manager
+                    .drop_type(Type::drop().name(LocationCategory).to_owned())
+                    .await?;
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum Location {
+    Table,
+    Id,
+    Name,
+    CreationTime,
+    Category,
+    Disabled,
+}
+
+#[derive(DeriveIden)]
+struct LocationCategory;
+
+#[derive(DeriveIden, EnumIter)]
+pub enum LocationCategoryVariant {
+    Dispenser,
+    Room,
+}
