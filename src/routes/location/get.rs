eb784c54a5a1cea87984b0300a9cffdaaa994337
flo-ride <43076999+flo-ride@users.noreply.github.com>
2024-10-30 11:33:11 +0100

    feat(routes) Add GET /location + /location/id routes

---
+++ src/routes/location/get.rs
@@ -0,0 +1,116 @@
+//! This module contains the route handler for retrieving location information.
+
+use crate::{
+    error::AppError,
+    models::{
+        profile::admin::Admin,
+        response::location::{LocationListResponse, LocationResponse},
+        utils::pagination::Pagination,
+    },
+};
+use axum::{
+    extract::{Path, Query, State},
+    Json,
+};
+use migration::IntoCondition;
+use sea_orm::ColumnTrait;
+use service::Connection;
+
+/// Handles the request to fetch a location by its unique identifier.
+///
+/// - **Path Parameters**:  
+///   `id` (UUID): The database ID of the location to retrieve.
+///
+/// - **Response Codes**:  
+///   - `200 OK`: The location was successfully retrieved.
+///   - `404 Not Found`: The location doesn't exist.
+///   - `400 Bad Request`: The request is improperly formatted.
+///   - `500 Internal Server Error`: An internal error, most likely related to the database, occurred.
+///
+/// - **Permissions**:  
+///   If the location is disabled, only an admin can retrieve it.
+#[utoipa::path(get, path = "/location/{id}", 
+               params(
+                   ("id" = uuid::Uuid, Path, description = "The database ID of the location to retrieve."),
+                ),
+                responses(
+                   (status = 500, description = "An internal error, most likely related to the database, occurred."), 
+                   (status = 400, description = "The request is improperly formatted."), 
+                   (status = 404, description = "The location doesn't exist, or is disabled and the requester is not an admin."), 
+                   (status = 200, description = "The location was successfully retrieved.", body = LocationResponse)
+                )
+            )]
+pub async fn get_location(
+    admin: Option<Admin>,
+    Path(id): Path<uuid::Uuid>,
+    State(conn): State<Connection>,
+) -> Result<Json<LocationResponse>, AppError> {
+    let result = service::Query::find_location_by_id(&conn, id).await?;
+
+    match result {
+        Some(location) => {
+            if location.disabled && admin.is_none() {
+                return Err(AppError::NotFound(format!(
+                    "The location with id: {id} doesn't exist"
+                )));
+            }
+            Ok(Json(location.into()))
+        }
+        None => Err(AppError::NotFound(format!(
+            "The location with id: {id} doesn't exist"
+        ))),
+    }
+}
+
+/// Handles the request to retrieve a paginated list of locations.
+///
+/// - **Query Parameters**:  
+///   - `page` (Optional, u64): The page index, default is 0.
+///   - `per_page` (Optional, u64): The number of location per page, default is 20.
+///
+/// - **Response Codes**:  
+///   - `200 OK`: Successfully retrieved a list of locations.
+///   - `400 Bad Request`: The request is improperly formatted.
+///   - `500 Internal Server Error`: An internal error, most likely related to the database, occurred.
+///
+/// - **Permissions**:  
+///   Only Admin can view disabled location
+#[utoipa::path(get, path = "/location",
+               params(
+                   ("page" = Option<u64>, Query, description = "The page index, default is 0"),
+                   ("per_page" = Option<u64>, Query, description = "The number of location per page, default is 20"),
+                ),
+                responses(
+                   (status = 500, description = "An internal error, most likely related to the database, occurred."), 
+                   (status = 400, description = "The request is improperly formatted."), 
+                   (status = 200, description = "Successfully retrieved a list of locations.", body = LocationListResponse)
+                )
+            )]
+pub async fn get_all_locations(
+    admin: Option<Admin>,
+    Query(pagination): Query<Pagination>,
+    State(conn): State<Connection>,
+) -> Result<Json<LocationListResponse>, AppError> {
+    let page = pagination.page.unwrap_or(0);
+    let per_page = pagination.per_page.unwrap_or(20);
+
+    let condition = if admin.is_some() {
+        service::every_condition().into_condition()
+    } else {
+        sea_orm::Condition::any().add(entity::location::Column::Disabled.eq(false))
+    };
+
+    let result =
+        service::Query::list_locations_with_condition(&conn, condition.clone(), page, per_page)
+            .await?;
+
+    let total_page =
+        (service::Query::count_locations_with_condition(&conn, condition).await? / per_page) + 1;
+
+    let locations = result.into_iter().map(Into::into).collect();
+    Ok(Json(LocationListResponse {
+        current_page: page,
+        total_page,
+        locations,
+    }))
+}
--- src/routes/location/mod.rs
+++ src/routes/location/mod.rs
@@ -1,7 +1,11 @@
 use axum::routing::{delete, get, post, put};
 
+pub mod get;
 pub mod new;
 
 pub fn router() -> axum::Router<crate::state::AppState> {
-    axum::Router::new().route("/", post(new::post_new_location))
+    axum::Router::new()
+        .route("/:id", get(get::get_location))
+        .route("/", get(get::get_all_locations))
+        .route("/", post(new::post_new_location))
 }
