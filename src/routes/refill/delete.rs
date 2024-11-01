08e07b7f44917de979a9ca958ed7bd72baa5e6ca
flo-ride <43076999+flo-ride@users.noreply.github.com>
2024-11-01 16:37:24 +0100

    feat(routes): Add DELETE /refill/id

---
+++ src/routes/refill/delete.rs
@@ -0,0 +1,60 @@
+//! This module defines the API endpoint to delete a refill by its ID.
+//!
+//! Only an admin can delete a refill.
+
+use crate::{error::AppError, models::profile::admin::Admin};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+};
+use service::Connection;
+
+/// Deletes a refill by its database ID.
+///
+/// The refill is not fully removed but marked as disabled in the database.
+/// Only an admin can perform this action.
+///
+/// - **Path Parameters:**
+///   - `id`: The unique ID of the refill in the database.
+///
+/// - **Responses:**
+///   - `500`: Internal error, likely related to the database.
+///   - `400`: The request format is invalid.
+///   - `200`: The refill has been successfully disabled.
+#[utoipa::path(delete, path = "/refill/{id}",
+               params(
+                   ("id" = uuid::Uuid, Path, description = "refill database id to delete refill for"),
+                ),
+               responses(
+                   (status = 500, description = "An internal error occured, probably databse related"), 
+                   (status = 400, description = "Your request is not correctly formatted"), 
+                   (status = 200, description = "The refill is disabled")
+                )
+               )]
+pub async fn delete_refill(
+    admin: Admin,
+    Path(id): Path<uuid::Uuid>,
+    State(conn): State<Connection>,
+) -> Result<impl IntoResponse, AppError> {
+    let result = service::Query::find_refill_by_id(&conn, id).await?;
+
+    match result {
+        Some(refill) => {
+            service::Mutation::delete_refill(&conn, id).await?;
+
+            tracing::info!(
+                "Admin {} \"{}\" just deleted the refill \"{}\" - {:?}",
+                admin.name,
+                admin.id,
+                id,
+                refill
+            );
+
+            Ok((StatusCode::OK, ""))
+        }
+        None => Err(AppError::NotFound(format!(
+            "The refill with id: {id} doesn't exist"
+        ))),
+    }
+}
--- src/routes/refill/mod.rs
+++ src/routes/refill/mod.rs
@@ -1,5 +1,6 @@
 use axum::routing::{delete, get, post, put};
 
+pub mod delete;
 pub mod edit;
 pub mod get;
 pub mod new;
@@ -10,4 +11,5 @@ pub fn router() -> axum::Router<crate::state::AppState> {
         .route("/", get(get::get_all_refills))
         .route("/", post(new::post_new_refill))
         .route("/:id", put(edit::edit_refill))
+        .route("/:id", delete(delete::delete_refill))
 }
