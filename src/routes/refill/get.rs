6972050a5a1ccfcc93fb268cad6ab93905a869d0
flo-ride <43076999+flo-ride@users.noreply.github.com>
2024-11-01 16:36:38 +0100

    feat(routes): Add GET /refill + /refill/id

---
+++ src/routes/refill/get.rs
@@ -0,0 +1,114 @@
+//! This module contains the route handler for retrieving refill information.
+
+use crate::{
+    error::AppError,
+    models::{profile::admin::Admin, utils::pagination::Pagination},
+};
+use axum::{
+    extract::{Path, Query, State},
+    Json,
+};
+use entity::response::refill::{RefillListResponse, RefillResponse, RefillResponseError};
+use migration::IntoCondition;
+use sea_orm::ColumnTrait;
+use service::Connection;
+
+/// Handles the request to fetch a refill by its unique identifier.
+///
+/// - **Path Parameters**:  
+///   `id` (UUID): The database ID of the refill to retrieve.
+///
+/// - **Response Codes**:  
+///   - `200 OK`: The refill was successfully retrieved.
+///   - `404 Not Found`: The refill doesn't exist.
+///   - `400 Bad Request`: The request is improperly formatted.
+///   - `500 Internal Server Error`: An internal error, most likely related to the database, occurred.
+///
+/// - **Permissions**:  
+///   If the refill is disabled, only an admin can retrieve it.
+#[utoipa::path(get, path = "/refill/{id}", 
+               params(
+                   ("id" = uuid::Uuid, Path, description = "The database ID of the refill to retrieve."),
+                ),
+                responses(
+                   (status = 500, description = "An internal error, most likely related to the database, occurred."), 
+                   (status = 400, description = "The request is improperly formatted."), 
+                   (status = 404, description = "The refill doesn't exist, or is disabled and the requester is not an admin."), 
+                   (status = 200, description = "The refill was successfully retrieved.", body = refillResponse)
+                )
+            )]
+pub async fn get_refill(
+    admin: Option<Admin>,
+    Path(id): Path<uuid::Uuid>,
+    State(conn): State<Connection>,
+) -> Result<Json<RefillResponse>, AppError> {
+    let result = service::Query::find_refill_by_id(&conn, id).await?;
+
+    match result {
+        Some(refill) => {
+            if refill.disabled && admin.is_none() {
+                return Err(AppError::NotFound(format!(
+                    "The refill with id: {id} doesn't exist"
+                )));
+            }
+            Ok(Json(refill.try_into()?))
+        }
+        None => Err(AppError::NotFound(format!(
+            "The refill with id: {id} doesn't exist"
+        ))),
+    }
+}
+
+/// Handles the request to retrieve a paginated list of refills.
+///
+/// - **Query Parameters**:  
+///   - `page` (Optional, u64): The page index, default is 0.
+///   - `per_page` (Optional, u64): The number of refill per page, default is 20.
+///
+/// - **Response Codes**:  
+///   - `200 OK`: Successfully retrieved a list of refills.
+///   - `400 Bad Request`: The request is improperly formatted.
+///   - `500 Internal Server Error`: An internal error, most likely related to the database, occurred.
+///
+/// - **Permissions**:  
+///   Only Admin can view disabled refill
+#[utoipa::path(get, path = "/refill",
+               params(
+                   ("page" = Option<u64>, Query, description = "The page index, default is 0"),
+                   ("per_page" = Option<u64>, Query, description = "The number of refill per page, default is 20"),
+                ),
+                responses(
+                   (status = 500, description = "An internal error, most likely related to the database, occurred."), 
+                   (status = 400, description = "The request is improperly formatted."), 
+                   (status = 200, description = "Successfully retrieved a list of refills.", body = refillListResponse)
+                )
+            )]
+pub async fn get_all_refills(
+    admin: Option<Admin>,
+    Query(pagination): Query<Pagination>,
+    State(conn): State<Connection>,
+) -> Result<Json<RefillListResponse>, AppError> {
+    let page = pagination.page.unwrap_or(0);
+    let per_page = pagination.per_page.unwrap_or(20);
+
+    let condition = if admin.is_some() {
+        service::every_condition().into_condition()
+    } else {
+        sea_orm::Condition::any().add(entity::models::refill::Column::Disabled.eq(false))
+    };
+
+    let result =
+        service::Query::list_refills_with_condition(&conn, condition.clone(), page, per_page)
+            .await?;
+
+    let total_page =
+        (service::Query::count_refills_with_condition(&conn, condition).await? / per_page) + 1;
+
+    let refills: Result<Vec<_>, RefillResponseError> =
+        result.into_iter().map(TryInto::try_into).collect();
+    Ok(Json(RefillListResponse {
+        current_page: page,
+        total_page,
+        refills: refills?,
+    }))
+}
--- src/routes/refill/mod.rs
+++ src/routes/refill/mod.rs
@@ -1,7 +1,11 @@
 use axum::routing::{delete, get, post, put};
 
+pub mod get;
 pub mod new;
 
 pub fn router() -> axum::Router<crate::state::AppState> {
-    axum::Router::new().route("/", post(new::post_new_refill))
+    axum::Router::new()
+        .route("/:id", get(get::get_refill))
+        .route("/", get(get::get_all_refills))
+        .route("/", post(new::post_new_refill))
 }
