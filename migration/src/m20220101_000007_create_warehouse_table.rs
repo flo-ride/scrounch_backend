dcd31799e10d80fbe02d6bf2c829fd6353bafdb4
flo-ride <43076999+flo-ride@users.noreply.github.com>
2025-02-21 18:25:32 +0100

    fix(migration): Add missing WarehouseProducts + WarehouseRecipes in m20220101_000007_create_warehouse_table migration

--- migration/src/m20220101_000007_create_warehouse_table.rs
+++ migration/src/m20220101_000007_create_warehouse_table.rs
@@ -1,5 +1,9 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::{
+    m20220101_000003_create_product_table::Product, m20220101_000006_create_recipe_table::Recipe,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
@@ -20,10 +24,78 @@ impl MigrationTrait for Migration {
                     )
                     .to_owned(),
             )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(WarehouseProducts::Table)
+                    .if_not_exists()
+                    .col(uuid(WarehouseProducts::WarehouseId))
+                    .col(uuid(WarehouseProducts::ProductId))
+                    .primary_key(
+                        Index::create()
+                            .col(WarehouseProducts::WarehouseId)
+                            .col(WarehouseProducts::ProductId),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(WarehouseProducts::Table, WarehouseProducts::WarehouseId)
+                            .to(Warehouse::Table, Warehouse::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(WarehouseProducts::Table, WarehouseProducts::ProductId)
+                            .to(Product::Table, Product::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(WarehouseRecipes::Table)
+                    .if_not_exists()
+                    .col(uuid(WarehouseRecipes::WarehouseId))
+                    .col(uuid(WarehouseRecipes::RecipeId))
+                    .primary_key(
+                        Index::create()
+                            .col(WarehouseRecipes::WarehouseId)
+                            .col(WarehouseRecipes::RecipeId),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(WarehouseRecipes::Table, WarehouseRecipes::WarehouseId)
+                            .to(Warehouse::Table, Warehouse::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(WarehouseRecipes::Table, WarehouseRecipes::RecipeId)
+                            .to(Recipe::Table, Recipe::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(WarehouseRecipes::Table).to_owned())
+            .await?;
+
+        manager
+            .drop_table(Table::drop().table(WarehouseProducts::Table).to_owned())
+            .await?;
+
         manager
             .drop_table(Table::drop().table(Warehouse::Table).to_owned())
             .await
@@ -37,4 +109,18 @@ pub enum Warehouse {
     Name,
     Disabled,
     CreatedAt,
+}
+
+#[derive(DeriveIden)]
+pub enum WarehouseProducts {
+    Table,
+    WarehouseId,
+    ProductId,
+}
+
+#[derive(DeriveIden)]
+pub enum WarehouseRecipes {
+    Table,
+    WarehouseId,
+    RecipeId,
 }
