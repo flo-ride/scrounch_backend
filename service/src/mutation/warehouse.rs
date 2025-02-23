a6801ba254762eb135b5d602a7e3c34b8340e76d
flo-ride <43076999+flo-ride@users.noreply.github.com>
2025-02-23 17:14:27 +0100

    fix(mutation): Add product_id in create_warehouse_product

--- service/src/mutation/warehouse.rs
+++ service/src/mutation/warehouse.rs
@@ -72,10 +72,12 @@ impl Mutation {
     pub async fn create_warehouse_product<M: IntoActiveModel<warehouse_product::ActiveModel>>(
         conn: &Connection,
         warehouse_id: uuid::Uuid,
+        product_id: uuid::Uuid,
         form_data: M,
     ) -> Result<warehouse_product::Model, DbErr> {
         let mut form_data = form_data.into_active_model();
         form_data.warehouse_id = Set(warehouse_id);
+        form_data.product_id = Set(product_id);
         let result = form_data.insert(&conn.db_connection).await;
 
         #[cfg(feature = "cache")]
