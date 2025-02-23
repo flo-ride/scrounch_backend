eb9189494aca7280bf4a0b3dbcfa1882cb70c79f
flo-ride <43076999+flo-ride@users.noreply.github.com>
2025-02-23 14:39:40 +0100

    feat(response): Add WarehouseProductsReponse

--- entity/src/response/warehouse.rs
+++ entity/src/response/warehouse.rs
@@ -2,6 +2,8 @@
 
 use serde_with::skip_serializing_none;
 
+use super::product::ProductResponse;
+
 /// Response structure for a warehouse, including its details.
 #[skip_serializing_none]
 #[derive(Debug, Clone, PartialEq, serde::Serialize, utoipa::ToSchema)]
@@ -12,10 +14,10 @@ use serde_with::skip_serializing_none;
 }))]
 pub struct WarehouseResponse {
     /// Unique identifier for the warehouse.
-    id: uuid::Uuid,
+    pub id: uuid::Uuid,
 
     /// Name of the warehouse.
-    name: String,
+    pub name: String,
 
     /// The timestamp indicating when the warehouse was created.
     pub created_at: chrono::DateTime<chrono::Utc>,
@@ -23,7 +25,6 @@ pub struct WarehouseResponse {
     /// Indicates whether the warehouse is currently disabled.
     pub disabled: bool,
 }
-
 impl From<crate::models::warehouse::Model> for WarehouseResponse {
     /// Constructs a WarehouseResponse from a warehouse model, returning an error if conversion fails.
     fn from(value: crate::models::warehouse::Model) -> Self {
@@ -61,4 +62,35 @@ pub struct WarehouseListResponse {
 
     /// List of warehouses on the current page.
     pub warehouses: Vec<WarehouseResponse>,
+}
+
+/// Represent a link between a Warehouse and a Product
+#[skip_serializing_none]
+#[derive(Debug, Clone, PartialEq, serde::Serialize, utoipa::ToSchema)]
+#[schema(example = json!({
+    "quantity": 10.5,
+    "created_at": "2024-02-23T14:00:00Z",
+}))]
+pub struct WarehouseProductResponse {
+    /// The product quantity in this warehouse
+    pub quantity: rust_decimal::Decimal,
+
+    /// The product of this Warehouse
+    pub product: ProductResponse,
+
+    /// The timestamp indicating when the link was created.
+    pub created_at: chrono::DateTime<chrono::Utc>,
+}
+
+/// Represent the lists of products for this Warehouse
+#[derive(Debug, Clone, PartialEq, serde::Serialize, utoipa::ToSchema)]
+pub struct WarehouseProductsListResponse {
+    /// The lists of products for this Warehouse
+    pub products: Vec<ProductResponse>,
+
+    /// Total number of pages available.
+    pub total_page: u64,
+
+    /// Current page number.
+    pub current_page: u64,
 }
